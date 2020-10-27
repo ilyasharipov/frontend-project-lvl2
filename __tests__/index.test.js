e46dce2a99d055fd9f4e79525df34139fd5bc200
Ilya Sharipov <ilyas@evaxsoft.com>
2020-10-27 18:51:40 +0700

    tests?

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,18 @@
 import { test, expect } from '@jest/globals';
-import reverse from '../src/index.js';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import genDiff from '../src/index'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const data1 = readFile('file2.json');
+const data2 = readFile('file2.json');
 
 test('reverse', () => {
-  // const str = 'hello';
-  // expect(reverse(str)).toEqual('olleh');
-  // expect(reverse('')).toEqual('');
+    genDiff(data1, data2);
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 export default (firstConfig, secondConfig, format = 'json') => {
   console.log(firstConfig, secondConfig, format);
 };
