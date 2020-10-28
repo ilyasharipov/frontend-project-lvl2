3bdf01243ae99e398bfff7b2e0c2d9b9fba6acce
Ilya Sharipov <ilyas@evaxsoft.com>
2020-10-28 14:26:11 +0700

    is it work, but not good dicide for flat files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,18 +1,20 @@
+/* eslint-disable no-underscore-dangle */
 import { test, expect } from '@jest/globals';
 import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import genDiff from '../src/index'
+import genDiff from '../src/index';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-const data1 = readFile('file2.json');
+
+const data1 = readFile('file1.json');
 const data2 = readFile('file2.json');
+const expected = readFile('expected');
 
 test('reverse', () => {
-    genDiff(data1, data2);
+  expect(genDiff(data1, data2, 'json')).toBe(expected);
 });
--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import _ from 'lodash';
 
-export default (firstConfig, secondConfig, format = 'json') => {
-  console.log(firstConfig, secondConfig, format);
+export default (beforeFile, afterFile, format = '') => {
+  const obj1 = JSON.parse(beforeFile);
+  const obj2 = JSON.parse(afterFile);
+
+  const keys = _.union(Object.keys(obj1), Object.keys((obj2))).sort();
+
+  const diff = keys.reduce((acc, key) => {
+
+    if (!_.has(obj1, key)) {
+      acc += '  + ' + key + ': ' + obj2[key] + '\n';
+    } else if (!_.has(obj2, key)) {
+      acc += '  - ' + key + ': ' + obj1[key] + '\n';
+    } else if (obj1[key] !== obj2[key]) {
+      acc += '  - ' + key + ': ' + obj1[key] + '\n';
+      acc += '  + ' + key + ': ' + obj2[key] + '\n';
+    } else if (obj1[key] === obj2[key]) {
+      acc += '    ' + key + ': ' + obj2[key] + '\n';
+    }
+
+    return acc;
+  }, '\n');
+
+  return '{' + diff + '}\n';
 };
