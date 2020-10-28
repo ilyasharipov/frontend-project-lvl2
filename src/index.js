191f4fef73370b90d73fa489d2f9a233291aa9bb
Ilya Sharipov <ilyas@evaxsoft.com>
2020-10-28 14:51:01 +0700

    explode string to arr like good dicede be

--- src/index.js
+++ src/index.js
@@ -7,20 +7,19 @@ export default (beforeFile, afterFile, format = '') => {
   const keys = _.union(Object.keys(obj1), Object.keys((obj2))).sort();
 
   const diff = keys.reduce((acc, key) => {
-
     if (!_.has(obj1, key)) {
-      acc += '  + ' + key + ': ' + obj2[key] + '\n';
+      acc.push(`  + ${key}: ${obj2[key]}\n`);
     } else if (!_.has(obj2, key)) {
-      acc += '  - ' + key + ': ' + obj1[key] + '\n';
+      acc.push(`  - ${key}: ${obj1[key]}\n`);
     } else if (obj1[key] !== obj2[key]) {
-      acc += '  - ' + key + ': ' + obj1[key] + '\n';
-      acc += '  + ' + key + ': ' + obj2[key] + '\n';
+      acc.push(`  - ${key}: ${obj1[key]}\n`);
+      acc.push(`  + ${key}: ${obj2[key]}\n`);
     } else if (obj1[key] === obj2[key]) {
-      acc += '    ' + key + ': ' + obj2[key] + '\n';
+      acc.push(`    ${key}: ${obj2[key]}\n`);
     }
 
     return acc;
-  }, '\n');
-
-  return '{' + diff + '}\n';
+  }, []);
+  console.log(format);
+  return `{\n${diff.join('')}}\n`;
 };
