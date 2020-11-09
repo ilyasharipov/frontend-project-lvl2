4084dc02d61ef5c2f95b87878693cf9d80230fc1
Ilya Sharipov <ilyas@evaxsoft.com>
2020-11-09 18:34:35 +0700

    lints

--- src/astBuilder.js
+++ src/astBuilder.js
@@ -1,36 +1,36 @@
 import _ from 'lodash';
 
-const buildNode = (key, beforeValue, afterValue, type, children) => {
-    return {
-        key: key,
-        beforeValue: beforeValue,
-        afterValue: afterValue,
-        type: type,
-        children: children,
-    };
-}
+const buildNode = (key, beforeValue, afterValue, type, children) => (
+  {
+    key,
+    beforeValue,
+    afterValue,
+    type,
+    children,
+  }
+);
 
 const getAst = (beforeFile, afterFile) => {
   const keys = _.union(Object.keys(beforeFile), Object.keys((afterFile))).sort();
 
   const ast = keys.reduce((acc, key) => {
-      if (!_.has(beforeFile, key)) {
-        acc.push(buildNode(key, null, afterFile[key], 'added', null));
-      } else if (!_.has(afterFile, key)) {
-        acc.push(buildNode(key, beforeFile[key], null, 'deleted', null));
-      } else if (_.has(beforeFile, key) && _.has(afterFile, key)) {
-          if (_.isObject(beforeFile[key]) && _.isObject(afterFile[key])) {
-            acc.push(buildNode(key, null, null, 'nested', getAst(beforeFile[key], afterFile[key])));
-          } else {
-              if (beforeFile[key] === afterFile[key]) {
-                  acc.push(buildNode(key, beforeFile[key], null, 'unchanged', null));
-              } else {
-                  acc.push(buildNode(key, beforeFile[key], afterFile[key], 'changed', null));
-              }
-          }
+    if (!_.has(beforeFile, key)) {
+      acc.push(buildNode(key, null, afterFile[key], 'added', null));
+    } else if (!_.has(afterFile, key)) {
+      acc.push(buildNode(key, beforeFile[key], null, 'deleted', null));
+    } else if (_.has(beforeFile, key) && _.has(afterFile, key)) {
+      if (_.isObject(beforeFile[key]) && _.isObject(afterFile[key])) {
+        acc.push(buildNode(key, null, null, 'nested', getAst(beforeFile[key], afterFile[key])));
+      } else {
+        if (beforeFile[key] === afterFile[key]) {
+          acc.push(buildNode(key, beforeFile[key], null, 'unchanged', null));
+        } else {
+          acc.push(buildNode(key, beforeFile[key], afterFile[key], 'changed', null));
+        }
       }
+    }
 
-      return acc;
+    return acc;
   }, []);
   return ast;
 };
