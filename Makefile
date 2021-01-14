77c48d5257b6bceb24bbea94a9478fd6b23aea4f
Ilya Sharipov <ilyas@evaxsoft.com>
2021-01-14 12:09:42 +0700

    improv configs

--- Makefile
+++ Makefile
@@ -10,3 +10,4 @@ test:
 	npm test
 test-coverage:
 	npm test -- --coverage --coverageProvider=v8
+.PHONY: test
--- src/utils.js
+++ src/utils.js
@@ -8,7 +8,6 @@ const __dirname = path.dirname(__filename);
 
 const getFilePath = (filename) => path.resolve(process.cwd(), filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-
 const readFile = (filename) => fs.readFileSync(getFilePath(filename), 'utf-8');
 const readFixtureFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
