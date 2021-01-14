ba8608d8abe3fa803eb22785d37a2dbc80b38d8d
Ilya Sharipov <ilyas@evaxsoft.com>
2021-01-14 14:43:57 +0700

    js?

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import parse from './parsers.js';
 import formatDiffTree from './formatters/index.js';
-import generateDiffTree from './astBuilder';
+import generateDiffTree from './astBuilder.js';
 
 const makeAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
 const getFormat = (filepath) => path.extname(filepath).slice(1);
